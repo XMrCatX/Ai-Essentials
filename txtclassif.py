7e621a02443ddaba2fb5e306a8cb4aa5415d307e
PILLE Ward <ward.pille@student.ehb.be>
2019-12-14 12:28:39 +0100

    loeding dataset amazon

--- txtclassif.py
+++ txtclassif.py
@@ -12,39 +12,10 @@ print("Eager mode: ", tf.executing_eagerly())
 print("Hub version: ", hub.__version__)
 print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
 
-train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
+PCrev_train, info = tfds.load(name="amazon_us_reviews/PC_v1_00", with_info=True , split="train")
+assert isinstance(PCrev_train, tf.data.Dataset)
+print(info)
 
-(train_data, validation_data), test_data = tfds.load(
-    name="amazon_us_reviews/PC_v1_00",
-    split=(train_validation_split, tfds.Split.TEST))
-
-print(test_data)
-
-train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
-train_examples_batch
-train_labels_batch
-
-embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
-hub_layer = hub.KerasLayer(embedding, input_shape=[],
-                           dtype=tf.string, trainable=True)
-hub_layer(train_examples_batch[:3])
-
-model = tf.keras.Sequential()
-model.add(hub_layer)
-model.add(tf.keras.layers.Dense(16, activation='relu'))
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-model.summary()
-
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit(train_data.shuffle(10000).batch(512),
-                    epochs=1000,
-                    validation_data=validation_data.batch(512),
-                    verbose=1)
-
-results = model.evaluate(test_data.batch(512), verbose=2)
-for name, value in zip(model.metrics_names, results):
-  print("%s: %.3f" % (name, value))
+for test_example in PCrev_train:  # Only take a single example
+  item, categorie, review, stars = test_example["data"]["product_title"], test_example["data"]["product_category"], test_example["data"]["review_body"], test_example["data"]["star_rating"]
+  print("naam: ", item.numpy(),"cat: ", categorie.numpy(),"review: ",review.numpy(),"stars: ",stars.numpy())
