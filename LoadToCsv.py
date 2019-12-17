eb17450f55e51e570584b5a7e30731416e1a0b62
XMrCatX <44335993+XMrCatX@users.noreply.github.com>
2019-12-17 17:58:01 +0100

    LoadDictToCsv done

---
+++ LoadToCsv.py
@@ -0,0 +1,38 @@
+# What version of Python do you have?
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+import sys
+import csv
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+from sklearn.model_selection import train_test_split
+
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tf.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+print(f"Pandas {pd.__version__}")
+print(f"Scikit-Learn {sk.__version__}")
+print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+print("Loading Dataset: amazon_us_reviews/PC_v1_00 from Tensorflow")
+Data, info = tfds.load(name="amazon_us_reviews/PC_v1_00", with_info=True, split="train")
+assert isinstance(Data, tf.data.Dataset)
+print("Dataset info: ", info)
+csv_file = "amazon_pc_user_reviews.csv"
+try:
+    with open(csv_file, 'w', encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["product_title","review_body","star_rating"])
+        for data in Data.take(60000):
+            item, review, stars = data["data"]["product_title"], data["data"]["review_body"], data["data"]["star_rating"]
+            writer.writerow([item.numpy().decode("utf-8"),review.numpy().decode("utf-8"),stars.numpy()])
+        print("Dataset saved with name: amazon_pc_user_reviews.csv")
+except IOError:
+    print("I/O error")
--- txtclassif.py
+++ txtclassif.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
@@ -15,27 +16,27 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 Data, info = tfds.load(name="amazon_us_reviews/PC_v1_00", with_info=True, split="train")
 assert isinstance(Data, tf.data.Dataset)
-print(info)
 
 for test_example in Data.take(1):  # Only take a single example
   item, categorie, review, stars = test_example["data"]["product_title"], test_example["data"]["product_category"], test_example["data"]["review_body"], test_example["data"]["star_rating"]
-  print("naam: ", item.numpy(),"cat: ", categorie.numpy(),"review: ",review.numpy(),"stars: ",stars.numpy())
+  print("naam: ", item.numpy().decode("utf-8"),"cat: ", categorie.numpy().decode("utf-8"),"review: ",review.numpy().decode("utf-8"),"stars: ",stars.numpy())
 
-#model.add(keras.layers.Embedding(10000,16))
-#model.add(keras.layers.GlobalAveragePooling1D())
-#model.add(keras.layers.Dense(16, activation = "relu"))
-#model.add(keras.layers.Dense(1, activation = "sigmoid"))
+model = keras.Sequential()
+model.add(keras.layers.Embedding(10000,16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation = "relu"))
+model.add(keras.layers.Dense(1, activation = "sigmoid"))
 
-#model.summary()
-#model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-#x_val = Data.range(10000)
-#x_train = Data.range(10000,20000)
+model.summary()
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+x_val = Data.range(10000)
+x_train = Data.range(10000,20000)
 
-#y_val = Data.range(10000)
-#y_train = Data.range(10000,20000)
+y_val = Data.range(10000)
+y_train = Data.range(10000,20000)
 
-#fitmodel= model.fit(x_train.shuffle(buffer_size = 10000,seed=None, reshuffle_each_iteration=None), epochs=40,validation_data=(x_val, y_val), verbose=1)
+fitmodel= model.fit(x_train.shuffle(buffer_size = 10000,seed=None, reshuffle_each_iteration=None), epochs=40,validation_data=(x_val, y_val), verbose=1)
 
-#result = model.evaulate(Data)
-#model.save("model.h5")
-#print(result)
+result = model.evaulate(Data)
+model.save("model.h5")
+print(result)
