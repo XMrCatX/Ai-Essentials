6d27b326bc81b3c2a10e926dd308def44121bdb3
PILLE Ward <ward.pille@student.ehb.be>
2019-12-26 00:52:26 +0100

    dawda

--- model_3_no_5_mse.py
+++ model_3_no_5_mse.py
@@ -18,7 +18,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from sklearn.linear_model import LogisticRegression
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-NUM_WORDS = 8000
+NUM_WORDS = 8000ada
 SEQ_LEN = 25
 maxlen = 256
 
